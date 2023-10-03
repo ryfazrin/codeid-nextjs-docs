44d7593d5fa9dc3ca5702216aead7817524c6996
riansera515@gmail.com <riansera515@gmail.com>
2023-10-03 14:11:05 +0700

    updated: footer copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,7 +127,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} CODE.ID, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
